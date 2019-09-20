c852a4fc8abf52d7917a6c14cc2f6ce849cadf27
Julien Morot <julien.morot@devinci.fr>
2019-09-20 10:37:11 +0200

    Commit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDCPVqp_j88RffzqmklV0I2lOCk-r5BdJ4",
+  authDomain: "angular-maps-253507.firebaseapp.com",
+  databaseURL: "https://angular-maps-253507.firebaseio.com",
+  projectId: "angular-maps-253507",
+  storageBucket: "",
+  messagingSenderId: "428109782556",
+  appId: "1:428109782556:web:d470ecb7948487028b09d9"
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tuto3';
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
