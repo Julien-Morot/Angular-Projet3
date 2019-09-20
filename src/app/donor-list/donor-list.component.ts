a7d19d4075c54250a97260f5f8d071867c15ea79
Julien Morot <julien.morot@devinci.fr>
2019-09-20 10:47:46 +0200

    Commit 4

--- src/app/donor-list/donor-list.component.ts
+++ src/app/donor-list/donor-list.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as firebase from 'firebase';
+
+declare var google: any;
+let map: any;
+let marker: any;
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+let infowindow: any;
+const iconBase = 'http://maps.google.com/mapfiles/ms/icons/';
+
+export const snapshotToArray = (snapshot: any) => {
+  const returnArr = [];
+
+  snapshot.forEach((childSnapshot: any) => {
+      const item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+  });
+
+  return returnArr;
+};
 
 @Component({
   selector: 'app-donor-list',
@@ -6,8 +30,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donor-list.component.scss']
 })
 export class DonorListComponent implements OnInit {
+  @ViewChild('map', {static: false}) mapElement: ElementRef;
+  donors = [];
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: { lat: location.coords.latitude, lng: location.coords.longitude },
+        zoom: 15
+      });
+  
+      infowindow = new google.maps.InfoWindow();
+  
+  
+      marker = new google.maps.Marker({
+        position: { lat: location.coords.latitude, lng: location.coords.longitude },
+        map,
+        title: 'Click to zoom',
+        icon: iconBase + 'blue-dot.png'
+      });
+  
+      map.addListener('center_changed', () => {
+        window.setTimeout(() => {
+          map.panTo(marker.getPosition());
+        }, 3000);
+      });
+  
+      marker.addListener('click', (event: any) => {
+        infowindow.setPosition(event.latLng);
+        infowindow.setContent('<h2>Yes, I wanna be a donor!</h2>' +
+        '<h3><a href="/add-donor/' + marker.getPosition().lat() + '/' + marker.getPosition().lng()  + '">Register Here</a></h3>');
+        infowindow.open(map, marker);
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+  }
 
-  constructor() { }
+  constructor() {
+    firebase.database().ref('donors/').on('value', resp => {
+      this.donors = [];
+      this.donors = snapshotToArray(resp);
+    });
+    this.initMap();
+  }
 
   ngOnInit() {
   }
