f42408f04e037c76822bf8f9d63f4ab1b09a7160
Julien Morot <julien.morot@devinci.fr>
2019-09-20 10:40:07 +0200

    Commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DonorListComponent } from './donor-list/donor-list.component';
+import { AddDonorComponent } from './add-donor/add-donor.component';
+
+const appRoutes: Routes = [
+  { path: 'donor-list', component: DonorListComponent },
+  { path: 'add-donor/:lat/:lng', component: AddDonorComponent },
+  { path: '',
+    redirectTo: '/donor-list',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DonorListComponent,
+    AddDonorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
